5f65394d4ea4aff41e3e090e5d43661eb4d49ba7
PhooPwintMaung <phoopwintmaung92@gmail.com>
2023-03-06 14:26:50 +0630

    commit2

--- homework.dart
+++ homework.dart
@@ -13,6 +13,7 @@ void main() {
 
   // Balance
   print('Enter Your Balance (Kyats)');
+
   int totalBalance = getInteger(
       amount: stdin.readLineSync(), retrunStr: "Enter Your Balance (Kyats)");
   print("Your balance is $totalBalance");
@@ -23,7 +24,7 @@ void main() {
       amount: stdin.readLineSync(), retrunStr: "Enter Your betAmount (Kyats)");
   print("Your betAmount is $totalBet");
 
-  if (totalBalance < totalBet) {
+  while (totalBalance < totalBet) {
     print(
         "You can't bet over amount of your Total Balance , TotalBalance:$totalBalance , betAmount: $totalBet");
     print('Enter Your betAmount (Kyats) Again !!!!');
@@ -32,10 +33,6 @@ void main() {
         retrunStr: "Enter Your betAmount (Kyats)");
     print("Your betAmount is $totalBet");
   }
-
-// Ramdon Number
-  int randomNumber = Random().nextInt(9) + 1;
-  // print(randomNumber);
 
   print('Choose a Number 1 to 9');
   int chooseNumber = getInteger(
@@ -46,31 +43,59 @@ void main() {
         amount: stdin.readLineSync(), retrunStr: "choose again 1 to 9");
   }
 
-  if (chooseNumber == randomNumber) {
-    print("Win");
-  } else {
-    print("lose");
-  }
-}
+  guess obj = guess(
+      name: username,
+      yourBalance: totalBalance,
+      betAmount: totalBet,
+      choosenNumber: chooseNumber);
 
-int getInteger({required String? amount, required String retrunStr}) {
-  while (amount == null ||
-      amount.trim().length == 0 ||
-      int.parse(amount).isNegative ||
-      int.parse(amount) == 0) {
-    print(retrunStr);
-    amount = stdin.readLineSync();
-  }
-  return int.parse(amount);
+  print(obj.winOrLose());
+  obj.test();
 }
 
 class guess {
   final String name;
-  final int yourBalance;
-  final int betAmount;
+  int yourBalance;
+  int betAmount;
+  int choosenNumber;
 
   guess(
       {required String this.name,
       required int this.yourBalance,
-      required int this.betAmount});
+      required int this.betAmount,
+      required int this.choosenNumber});
+
+// Ramdon Number
+  int randomNumber = Random().nextInt(9) + 1;
+
+  String winOrLose() {
+    if (randomNumber - 2 <= choosenNumber &&
+        randomNumber + 2 >= choosenNumber) {
+      return "win";
+    } else {
+      return "lose";
+    }
+  }
+
+  void test() {
+    if (winOrLose() == "win") {
+      yourBalance = yourBalance + betAmount;
+      print("Your balance is $yourBalance");
+    } else {
+      yourBalance = yourBalance - betAmount;
+      print("Your balance is $yourBalance");
+    }
+  }
+}
+
+// Function For Getting Integer
+int getInteger({required String? amount, required String retrunStr}) {
+  while (amount == null ||
+      amount.trim().length == 0 ||
+      int.parse(amount.replaceAll(new RegExp(r'[^0-9]'), '')).isNegative ||
+      int.parse(amount.replaceAll(new RegExp(r'[^0-9]'), '')) == 0) {
+    print(retrunStr);
+    amount = stdin.readLineSync();
+  }
+  return int.parse(amount.replaceAll(new RegExp(r'[^0-9]'), ''));
 }
